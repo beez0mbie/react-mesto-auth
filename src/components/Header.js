604607ea76fac13ed058d9c6dee11b1a5b781cda
beez0mbie <vzhik3268@gmail.com>
2023-07-16 17:24:23 +0300

    Make header styles for desktop and mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,22 @@ import logo from '../images/Logo.svg';
 const Header = () => {
   return (
     <header className="header">
-      <img
-        src={logo}
-        alt="Логотип 'Место'"
-        className="header__logo"
-      />
+      <div className="header__logo-container">
+        <img
+          src={logo}
+          alt="Логотип 'Место'"
+          className="header__logo"
+        />
+        <button className="header__hamburger header__hamburger_active"></button>
+      </div>
+      <div className="header__auth-container header__auth-container_active">
+        <p className="header__email">email@some.com</p>
+        <a
+          className="header__action"
+          href="/login">
+          Выйти
+        </a>
+      </div>
     </header>
   );
 };
