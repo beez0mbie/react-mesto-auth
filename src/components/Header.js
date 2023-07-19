1ca4e7c8e7580fca57f0cc70db57596b3816945b
beez0mbie <vzhik3268@gmail.com>
2023-07-19 12:18:18 +0300

    realize header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../images/Logo.svg';
 import { Link, useMatch } from 'react-router-dom';
 import { CurrentUserContext } from '../contexts';
 
 const Header = ({ isLoggedIn, handleExit }) => {
+  const curentUser = React.useContext(CurrentUserContext);
+  const [isHamburgerActive, setIsHamburgerActive] = useState(false);
   const signUp = useMatch('sign-up');
   const signIn = useMatch('sign-in');
-  const curentUser = React.useContext(CurrentUserContext);
   return (
     <header className="header">
       <div className="header__logo-container">
@@ -15,9 +16,15 @@ const Header = ({ isLoggedIn, handleExit }) => {
           alt="Логотип 'Место'"
           className="header__logo"
         />
-        <button className="header__hamburger header__hamburger_active"></button>
+        <button
+          onClick={() => setIsHamburgerActive((prevState) => !prevState)}
+          className={`header__hamburger ${isHamburgerActive && 'header__hamburger_active'}`}
+        />
       </div>
-      <div className="header__auth-container header__auth-container_active">
+      <div
+        className={`header__auth-container ${
+          isHamburgerActive && 'header__auth-container_active'
+        }`}>
         {isLoggedIn && (
           <>
             <p className="header__email">{curentUser.email}</p>
